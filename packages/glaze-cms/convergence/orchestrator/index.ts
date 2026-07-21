a6c006d576b545f9893f62307fd9b35a099739a9
leanhanc <leanhanc@gmail.com>
2026-07-21 11:21:57 -0300

    chore(convergence): rename convergence file name and update exports

--- packages/glaze-cms/convergence/orchestrator/index.ts
+++ packages/glaze-cms/convergence/orchestrator/index.ts
@@ -1,8 +1,8 @@
-export { converge } from './converge.ts';
+export { converge } from './engine.ts';
 export { createGenerateCompute } from './generate.ts';
 export { resolveWithDecisions } from './resolution.ts';
 
-export type { ConvergeOptions, ConvergeResult, DropConfirmer, LossResolver } from './converge.ts';
+export type { ConvergeOptions, ConvergeResult, DropConfirmer, LossResolver } from './engine.ts';
 export type { GenerateConfig } from './generate.ts';
 export type { ResolveOptions } from './resolution.ts';
 export type {
